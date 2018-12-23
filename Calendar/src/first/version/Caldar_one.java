f324febf2b9b0d31b81233cf5eaba817474c24ef
jae57 <ggapsong@naver.com>
2018-12-23 14:51:25 +0900

    Calendar 출력만

---
+++ Calendar/src/first/version/Caldar_one.java
@@ -0,0 +1,64 @@
+package first.version;
+
+import java.util.ArrayList;
+
+public class Caldar_one {
+    ArrayList<String> cal = new ArrayList<>();
+    int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+    String year;
+    int space;
+    /// 1.
+    /// 년도("2019") 입력시 해당 년도 모든 달 출력
+
+    // 첫 달은 무조건 1월이니까 maxDay = 31
+    // 해당년도의 1월 1일이 무슨 요일인지만 알면됨(특히, index!!)
+
+    Caldar_one(String year, int space){
+        this.year = year;
+        this.space = space;
+    }
+
+    public void makeCal() {
+
+        for(int i=0;i<space;i++){
+            cal.add("  ");
+        }
+
+        for(int maxDay : maxDays){
+
+            for(int i=1;i<=maxDay;i++){
+                cal.add(String.valueOf(i));
+            }
+
+            for(int i=0;i<7;i++){
+                cal.add("  ");
+            }
+
+        }
+
+    }
+
+    public void printCal(){
+        int isEnter = 6;
+        int monthCount = 1;
+        System.out.println(year+"년도 "+monthCount+"월");
+        System.out.println("일 월 화 수 목 금 토");
+        for(int i=0;i<cal.size();i++){
+            System.out.printf("%2s",cal.get(i));
+            System.out.print(" ");
+            if(i==isEnter){
+                System.out.println();
+                isEnter+=7;
+                if((cal.get(i)).equals("  ")){
+                    monthCount++;
+                    if(monthCount>12)
+                        break;
+                    System.out.println();
+                    System.out.println(year+"년도 "+monthCount+"월");
+                    System.out.println("일 월 화 수 목 금 토");
+                }
+            }
+        }
+    }
+
+}
---
+++ Calendar/src/first/version/Caldar_three.java
@@ -0,0 +1,73 @@
+package first.version;
+
+import java.util.ArrayList;
+
+public class Caldar_three {
+    ArrayList<String> cal = new ArrayList<>();
+    int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+    String year;
+    String month;
+    String day;
+    int space;
+
+    int monthN;
+    int dayN;
+
+    Caldar_three(String printDate, int space){
+        this.year = printDate.substring(0,4);
+        this.month = printDate.substring(4,6);
+        this.day = printDate.substring(6,8);
+        this.space = space;
+
+        monthN = Integer.parseInt(month);
+        dayN = Integer.parseInt(day);
+    }
+
+    public void makeCal() {
+
+        int[] maxDays2 = new int[12-(monthN-1)];
+        System.arraycopy(maxDays, monthN-1, maxDays2, 0, maxDays2.length);
+
+        for(int i=0;i<space;i++){
+            cal.add("  ");
+        }
+
+        for(int maxDay : maxDays2){
+
+            for(int i=dayN;i<=maxDay;i++){
+                cal.add(String.valueOf(i));
+            }
+
+            for(int i=0;i<7;i++){
+                cal.add("  ");
+            }
+
+        }
+
+    }
+
+
+    public void printCal(){
+        int isEnter = 6;
+        int monthCount = monthN;
+        System.out.println(year+"년도 "+monthCount+"월");
+        System.out.println("일 월 화 수 목 금 토");
+        for(int i=0;i<cal.size();i++){
+            System.out.printf("%2s",cal.get(i));
+            System.out.print(" ");
+            if(i==isEnter){
+                System.out.println();
+                isEnter+=7;
+                if((cal.get(i)).equals("  ")){
+                    monthCount++;
+                    if(monthCount>12)
+                        break;
+                    System.out.println();
+                    System.out.println(year+"년도 "+monthCount+"월");
+                    System.out.println("일 월 화 수 목 금 토");
+                }
+            }
+        }
+    }
+
+}
