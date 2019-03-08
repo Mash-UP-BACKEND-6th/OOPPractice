d9b4ae39b6567632157d27c17c1c98c127367a37
changmin <changmin.kim@worksmobile.com>
2019-03-08 21:09:25 +0900

    [ADD] 상품 추가할때 다른 상품, 돈부족한 경우 추가

--- changmin/VendingMachine/src/test/java/VendingMachineTest.java
+++ changmin/VendingMachine/src/test/java/VendingMachineTest.java
@@ -31,13 +31,45 @@ public class VendingMachineTest {
     //상품 추가 및 확인
     @Test
     public void test_addProduct() {
+        //프로덕트의 속성 별(4개)로 상품을 구별한다. 하나라도 다르면 이건 다른 상품(isEqual)-> 정책문제
         Product test1 = new Product("test","test",0,0, new Date(System.currentTimeMillis()));
         Product test2 = new Product("test","test",0,0, new Date(System.currentTimeMillis()));
 
         vendingMachine.addStock(test1);
         vendingMachine.addStock(test2);
         List<ProductInfo> productInfoList = vendingMachine.getProductList();
+
+        Assert.assertTrue(productInfoList.size() == 1);
         Assert.assertEquals(new ProductInfo(test1), productInfoList.get(0));
+        Assert.assertEquals(new ProductInfo(test2), productInfoList.get(0));
+
+        Product test3 = new Product("test", "test3", 0, 0, new Date(System.currentTimeMillis()));
+        vendingMachine.addStock(test3);
+
+        List<ProductInfo> nextProductInfoList = vendingMachine.getProductList();
+        Assert.assertTrue(nextProductInfoList.size() == 2);
+        Assert.assertEquals(new ProductInfo(test3), nextProductInfoList.get(1));
+
+        Product test4 = new Product("test4", "test", 0 ,0, new Date(System.currentTimeMillis()));
+        vendingMachine.addStock(test4);
+
+        nextProductInfoList = vendingMachine.getProductList();
+        Assert.assertTrue(nextProductInfoList.size() == 3);
+        Assert.assertEquals(new ProductInfo(test4), nextProductInfoList.get(2));
+
+        Product test5 = new Product("test", "test", 1, 0, new Date(System.currentTimeMillis()));
+        vendingMachine.addStock(test5);
+        nextProductInfoList = vendingMachine.getProductList();
+
+        Assert.assertTrue(nextProductInfoList.size() == 4);
+        Assert.assertEquals(new ProductInfo(test5), nextProductInfoList.get(3));
+
+        //비싸서 못삼
+        Product test6 = new Product("test", "test", 0, 1, new Date(System.currentTimeMillis()));
+        vendingMachine.addStock(test6);
+        nextProductInfoList = vendingMachine.getProductList();
+
+        Assert.assertTrue(nextProductInfoList.size() == 4);
     }
 
     // 상품 구매하기
@@ -74,7 +106,6 @@ public class VendingMachineTest {
         List<Product> records = vendingMachine.getRecords();
         Product record = records.get(0);
         Assert.assertEquals(test, record);
-
     }
 
 }
